171d21ae679995c4da6b9edc6cd59bfeb2bf6a57
pierrialexandervidmar <pierri@hotmail.com.br>
2024-03-01 21:50:03 -0300

    Continuidade no TransactionService

---
+++ src/main/java/com/picpaysimplificado/services/UserService.java
@@ -0,0 +1,36 @@
+package com.picpaysimplificado.services;
+
+import com.picpaysimplificado.domain.user.User;
+import com.picpaysimplificado.domain.user.UserType;
+import com.picpaysimplificado.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public void validateTransaction(User sender, BigDecimal amount) throws Exception {
+        // ser for do tipo Logista, retorna uma exception
+        if (sender.getUserType() == UserType.MERCHANT) {
+            throw new Exception("Usuário do tipo Logista não está autorizado a realizar transação");
+        }
+
+        // Valida se tem saldo suficiente para efetuar a transação
+        if (sender.getBalance().compareTo(amount) < 0) {
+            throw new Exception("Saldo Insuficiente");
+        }
+    }
+
+    public User findUserById(Long id) throws Exception {
+        return this.repository.findUserById(id).orElseThrow(() -> new Exception("Usuário não encontrado"));
+    }
+
+    public void saveUser(User user) {
+        this.repository.save(user);
+    }
+}
